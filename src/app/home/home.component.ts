efc0afd043d97dac1244f19ce49703feb94e85b4
Dowon Kim <dowoniscool@yahoo.com>
2024-05-27 10:15:02 +0200

    feat: Fetching Project From Server (Services & Endpoints)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { ProductsService } from '../services/products.service';
+import { Router } from '@angular/router';
+import { Products } from '../../types';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  constructor(
+    private productsService: ProductsService,
+    private router: Router
+  ) {}
 
+  ngOnInit() {
+    this.productsService
+      .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
+      .subscribe((products: Products) => {
+        console.log(products.items);
+      });
+  }
 }
